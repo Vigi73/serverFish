b03cca3bea23dc7e1ce6a44bed16b0d12c5e25f7
Egioya Evgeniy <vigi73@mail.ru>
2021-12-14 11:48:54 +0700

    edit err

--- main.py
+++ main.py
@@ -13,28 +13,28 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\t
 
 x, y = bot.size()
 
-x = (x // 2) - 402
-y = (y // 2) - 319
+x = (x // 2) - 401
+y = (y // 2) - 320
 
-x_value = x + 32
-y_value = y + 44
+x_value = x + 31
+y_value = y + 43
 
 
 def fish_tank():
     global x, y, x_value, y_value
-    if bot.locateOnScreen(r'img\tank.png', region=(x, y, x + 112, y + 35)):
-        img = bot.screenshot('my_screenshot.png', region=(x_value, y_value, 329, 151))
+    if bot.locateOnScreen(r'img\tank.png', region=(x, y, x + 111, y + 36)):
+        img = bot.screenshot('my_screenshot.png', region=(x_value, y_value, 330, 152))
+        bot.sleep(.2)
         answer = pytesseract.image_to_string(img, lang='rus').split('\n')[:4]
-        bot.sleep(.2)
         bot.press('space')
-        bot.sleep(.2)
+        bot.sleep(.5)
         return answer
     else:
         pass
 
 
 tmp_fish = ""
-
+err = 0
 while True:
     if "RF3.exe" in [p.name() for p in pu.process_iter()]:
 
@@ -43,18 +43,18 @@ while True:
                 try:
                     fish = ans[0]
                 except:
-                    fish = "error"
+                    err += 1
                 try:
                     width_f = ans[1].split(':')[1].split()[0].strip().replace(",", '.')
                 except:
-                    width_f = "error"
+                    err += 1
                 try:
                     if "зачетная" in ans[1]:
                         top = "зачетная"
                     else:
                         top = "мелочь"
                 except:
-                    top = "error"
+                    err += 1
 
                 try:
                     bait = ans[2].split(':')[1].strip()
@@ -63,17 +63,20 @@ while True:
                 try:
                     base = ans[3].split(":")[1].split(',')[0].strip()
                 except IndexError:
-                    base = ""
+                    err += 1
 
                 try:
                     location = ans[3].split(":")[1].split(',')[1].strip()
                 except IndexError:
-                    location = ""
+                    err += 1
 
-                with open('log.txt', 'a', encoding='utf-8') as f:
-                    print(f'{fish}; {width_f}; {top}; {bait}; {base}; {location}', file=f)
-                    # print(f'{fish}; {width_f}; {top}; {bait}; {base}; {location}')
-                tmp_fish = str(ans)
+                if err == 0:
+                    with open('log.txt', 'a', encoding='utf-8') as f:
+                        print(f'{fish}; {width_f}; {top}; {bait}; {base}; {location}', file=f)
+                        print(f'{fish}; {width_f}; {top}; {bait}; {base}; {location}')
+                    tmp_fish = str(ans)
+                else:
+                    err = 0
 
     else:
         bot.alert('Запустите рыбалку')
